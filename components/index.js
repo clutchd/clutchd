0fc0b2403845aff1822109cbf26aa41f96ae3c6d
William Crutchfield <william.r.crutchfield@gmail.com>
2019-09-08 20:44:01 -0700

    export app & grid components

--- components/index.js
+++ components/index.js
@@ -1,3 +1,5 @@
+export { default as App } from './app';
 export { default as Button } from './button';
+export { default as Grid } from './grid';
 export { default as Icon } from './icon';
 export { default as Typography } from './typography';
