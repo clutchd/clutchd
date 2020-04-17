e79b50b64ba807c97227ada7eaeea4fc04e1470e
William Crutchfield <william.r.crutchfield@gmail.com>
2020-04-16 23:42:20 -0400

    MDX Docs

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    {
+      name: "storybook-addon-deps/preset",
+      options: {
+        exclude: /^@babel/,
+      },
+    },
+    {
+      name: "@storybook/preset-create-react-app",
+    },
+  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.js"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-docs",
+    "@storybook/addon-actions",
+    "@storybook/addon-storysource",
+    "@storybook/addon-a11y",
+  ],
+};
--- src/components/button/button.stories.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import Button from "../button";
-
-storiesOf("Button", module)
-  .addParameters({ component: Button })
-  .add("Default", () => <Button>Test</Button>);
