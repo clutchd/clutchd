855cefece368f94a910c144a7c01b5d4e84bcf63
William Crutchfield <william.r.crutchfield@gmail.com>
2021-06-09 23:27:29 -0400

    remove @storybook/addon-postcss

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,6 @@ module.exports = {
     "@storybook/addon-actions",
     "@storybook/addon-backgrounds",
     "@storybook/addon-docs",
-    "@storybook/addon-postcss",
   ],
   typescript: {
     reactDocgen: "none",
