1b2d3c8b9e54d80751d4870a120da8b405836463
William Crutchfield <william.r.crutchfield@gmail.com>
2021-06-21 22:23:11 -0400

    fix(navbutton): export navbutton

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { IComponent, Component } from "./component";
 export { IButton, Button } from "./button";
 export { ILink, Link } from "./link";
+export { INavButton, NavButton } from "./navButton";
