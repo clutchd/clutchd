197d5d2d2feebb8603c010acba8a6e5feb9237eb
William Crutchfield <william.r.crutchfield@gmail.com>
2021-07-23 15:59:42 -0400

    feat(input field): a input field component

---
+++ src/forms/inputField/index.ts
@@ -0,0 +1,1 @@
+export { default as InputField, IInputField } from "./inputField";
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { Button, IButton } from "./button";
 export { Component, IComponent } from "./component";
+export { IInputField, InputField } from "./forms/inputField";
 export { ILink, Link } from "./link";
 export { INavButton, NavButton } from "./navButton";
