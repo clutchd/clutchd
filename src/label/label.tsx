cd3a70077fc855ed4e6f7f5fd6566e178410f055
William Crutchfield <william.r.crutchfield@gmail.com>
2021-07-25 22:31:24 -0400

    Add required decorator to label

--- src/label/label.tsx
+++ src/label/label.tsx
@@ -5,9 +5,11 @@ export interface ILabel
   extends React.DetailedHTMLProps<
     React.LabelHTMLAttributes<HTMLLabelElement>,
     HTMLLabelElement
-  > {}
+  > {
+  required?: boolean;
+}
 
-export default function Label({ ...props }: ILabel) {
+export default function Label({ required, ...props }: ILabel) {
   return (
     <label
       {...props}
@@ -15,6 +17,11 @@ export default function Label({ ...props }: ILabel) {
         props.className,
         "block w-full text-base font-medium transition-all outline-none text-gray-900 py-1"
       )}
-    />
+    >
+      {props.children}
+      {required ? (
+        <span className="float-right text-red-600 font-light">*</span>
+      ) : null}
+    </label>
   );
 }
