dcab99c142c65624ba096c2fafe05348be15c7bb
William Crutchfield <william.r.crutchfield@gmail.com>
2022-01-02 12:14:35 -0800

    fix(link): fix motion export that caused link storybook to fail

--- src/link/link.tsx
+++ src/link/link.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
-import { HTMLMotionProps, motion } from "framer-motion";
+import { HTMLMotionProps } from "framer-motion";
+import { motion } from "framer-motion/dist/framer-motion";
 import NextLink from "next/link";
 import React from "react";
 
