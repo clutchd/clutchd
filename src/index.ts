9d7adba0855842d8c9c87f80ab161563612314fa
William Crutchfield <william.r.crutchfield@gmail.com>
2022-08-09 20:42:03 -0400

    init turborepo

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export { Component, IBaseComponent, IComponent } from "./component";
-export { Field, Form, IField, ILabel, Label } from "./form";
-export { ILink, Link } from "./link";
-export { INavigator, Navigator } from "./navigator";
-export { IPlaceholder, Placeholder } from "./placeholder";
-export { IsCurrentRoute } from "./utils";
--- src/utils/isCurrentRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { NextRouter } from "next/router";
-
-export default function isCurrentRoute(router: NextRouter, href: string | undefined) {
-  href = href?.startsWith("/") ? href : `/${href}`;
-  if (router?.route.startsWith(href)) {
-    return true;
-  }
-  return false;
-}
