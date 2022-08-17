3e4aac69ef0172e3704ed76939b1fb4690d4d773
William Crutchfield <william.r.crutchfield@gmail.com>
2022-08-17 01:45:52 -0400

    remove extraneous packages

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
-  root: true,
-  extends: ["clutchd"],
+  extends: ["next", "turbo", "prettier"],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "react/jsx-key": "off",
+  },
 };
