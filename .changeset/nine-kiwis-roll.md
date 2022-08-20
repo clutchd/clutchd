c560f994b37f1e1ba9a02ecd87c369e7cf70d966
William Crutchfield <william.r.crutchfield@gmail.com>
2022-08-20 17:28:57 -0400

    Improve padding consistency

---
+++ .changeset/nine-kiwis-roll.md
@@ -0,0 +1,5 @@
+---
+"@clutchd/layout": patch
+---
+
+Improve padding consistency
--- packages/layout/src/content.tsx
+++ packages/layout/src/content.tsx
@@ -4,7 +4,7 @@ import clsx from "clsx";
 function Content({
   ...props
 }: ComponentPropsWithoutRef<typeof Component.main>) {
-  const className = clsx("flex flex-col flex-1", props.className);
+  const className = clsx("flex flex-col flex-1 flex p-6 sm:p-8", props.className);
   return <main {...props} className={className} />;
 }
 
