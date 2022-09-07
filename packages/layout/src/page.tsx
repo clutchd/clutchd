c2fd46ada99d78647557d9a2c93efa016d021ae7
William Crutchfield <william.r.crutchfield@gmail.com>
2022-09-07 00:15:47 -0400

    Remove flex-col from Layout.Page

--- packages/layout/src/page.tsx
+++ packages/layout/src/page.tsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 
 function Page({ ...props }: ReactPropsWithoutRef<typeof Component.div>) {
   const className = clsx(
-    "flex flex-col flex-1 min-h-screen min-w-screen",
+    "flex flex-1 min-h-screen min-w-screen",
     props.className
   );
   return <div {...props} className={className} />;
