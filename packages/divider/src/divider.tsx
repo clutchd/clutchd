0e1ae329300b3c568d756ffd6527d8c5c1b300fc
William Crutchfield <william.r.crutchfield@gmail.com>
2022-10-11 22:15:38 -0400

    add divider comments

--- packages/divider/src/divider.tsx
+++ packages/divider/src/divider.tsx
@@ -1,16 +1,26 @@
+/**
+ * Type to define `Divider` component
+ */
 type IDivider = typeof Divider;
 
+/**
+ * Type to define `Divider` props
+ */
 interface IDividerProps {
   hr?: boolean;
 }
 
+/**
+ * `Divider` - A separator used to create vertical space between elements
+ * @param props `IDividerProps` used to render this `Divider`
+ * @returns `Divider` component
+ */
 function Divider({ hr = true, ...props }: IDividerProps) {
   return (
     <div
       {...props}
-      className={`w-full mx-auto mt-6 mb-6 rounded-lg h-[3px] ${
-        hr ? "bg-gray-200" : "bg-inherit"
-      }`}
+      className={`w-full mx-auto mt-6 mb-6 rounded-lg h-[3px] ${hr ? "bg-gray-200" : "bg-inherit"
+        }`}
     />
   );
 }
