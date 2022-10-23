ab36fff6a34b173a9d2a63b19ca38a397a214199
William Crutchfield <william.r.crutchfield@gmail.com>
2022-10-23 15:23:35 -0400

    Add colSpan & rowSpan

---
+++ packages/tailwind/src/colSpan.tsx
@@ -0,0 +1,86 @@
+/**
+ * {@link https://tailwindcss.com/docs/grid-column}
+ */
+
+export const colSpan = [
+  "col-auto",
+  "col-span-1",
+  "col-span-2",
+  "col-span-3",
+  "col-span-4",
+  "col-span-5",
+  "col-span-6",
+  "col-span-7",
+  "col-span-8",
+  "col-span-9",
+  "col-span-10",
+  "col-span-11",
+  "col-span-12",
+  "col-span-full",
+] as const;
+
+export const colSpanSm = [
+  "sm:col-auto",
+  "sm:col-span-1",
+  "sm:col-span-2",
+  "sm:col-span-3",
+  "sm:col-span-4",
+  "sm:col-span-5",
+  "sm:col-span-6",
+  "sm:col-span-7",
+  "sm:col-span-8",
+  "sm:col-span-9",
+  "sm:col-span-10",
+  "sm:col-span-11",
+  "sm:col-span-12",
+  "sm:col-span-full",
+] as const;
+
+export const colSpanMd = [
+  "md:col-auto",
+  "md:col-span-1",
+  "md:col-span-2",
+  "md:col-span-3",
+  "md:col-span-4",
+  "md:col-span-5",
+  "md:col-span-6",
+  "md:col-span-7",
+  "md:col-span-8",
+  "md:col-span-9",
+  "md:col-span-10",
+  "md:col-span-11",
+  "md:col-span-12",
+  "md:col-span-full",
+] as const;
+
+export const colSpanLg = [
+  "lg:col-auto",
+  "lg:col-span-1",
+  "lg:col-span-2",
+  "lg:col-span-3",
+  "lg:col-span-4",
+  "lg:col-span-5",
+  "lg:col-span-6",
+  "lg:col-span-7",
+  "lg:col-span-8",
+  "lg:col-span-9",
+  "lg:col-span-10",
+  "lg:col-span-11",
+  "lg:col-span-12",
+  "lg:col-span-full",
+] as const;
+
+export type IColSpan = typeof colSpan[number];
+
+export type IColSpanSm = typeof colSpanSm[number];
+
+export type IColSpanMd = typeof colSpanMd[number];
+
+export type IColSpanLg = typeof colSpanLg[number];
+
+export interface IWithColSpan {
+  colSpan?: IColSpan;
+  colSpanSm?: IColSpanSm;
+  colSpanMd?: IColSpanMd;
+  colSpanLg?: IColSpanLg;
+}
---
+++ packages/tailwind/src/rowSpan.tsx
@@ -0,0 +1,62 @@
+/**
+ * {@link https://tailwindcss.com/docs/grid-row}
+ */
+
+export const rowSpan = [
+  "row-auto",
+  "row-span-1",
+  "row-span-2",
+  "row-span-3",
+  "row-span-4",
+  "row-span-5",
+  "row-span-6",
+  "row-span-full",
+] as const;
+
+export const rowSpanSm = [
+  "sm:row-auto",
+  "sm:row-span-1",
+  "sm:row-span-2",
+  "sm:row-span-3",
+  "sm:row-span-4",
+  "sm:row-span-5",
+  "sm:row-span-6",
+  "sm:row-span-full",
+] as const;
+
+export const rowSpanMd = [
+  "md:row-auto",
+  "md:row-span-1",
+  "md:row-span-2",
+  "md:row-span-3",
+  "md:row-span-4",
+  "md:row-span-5",
+  "md:row-span-6",
+  "md:row-span-full",
+] as const;
+
+export const rowSpanLg = [
+  "lg:row-auto",
+  "lg:row-span-1",
+  "lg:row-span-2",
+  "lg:row-span-3",
+  "lg:row-span-4",
+  "lg:row-span-5",
+  "lg:row-span-6",
+  "lg:row-span-full",
+] as const;
+
+export type IRowSpan = typeof rowSpan[number];
+
+export type IRowSpanSm = typeof rowSpanSm[number];
+
+export type IRowSpanMd = typeof rowSpanMd[number];
+
+export type IRowSpanLg = typeof rowSpanLg[number];
+
+export interface IWithRowSpan {
+  rowSpan?: IRowSpan;
+  rowSpanSm?: IRowSpanSm;
+  rowSpanMd?: IRowSpanMd;
+  rowSpanLg?: IRowSpanLg;
+}
