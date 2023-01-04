0b0ecb47efdb76fece896ac1ede407bb5f7cdfbd
William Crutchfield <william.r.crutchfield@gmail.com>
2023-01-04 18:41:22 -0500

    Add readme to get-initials

---
+++ packages/get-initials/README.md
@@ -0,0 +1,9 @@
+# @clutchd/get-initials
+
+A small utility intended to get initials that represent the provided names.
+
+## Installation
+
+```sh
+npm i @clutchd/get-initials
+```
--- packages/get-initials/src/getInitials.tsx
+++ packages/get-initials/src/getInitials.tsx
@@ -1,19 +1,24 @@
 import { isEmpty } from "@clutchd/is-empty";
 
-function getInitials(...args: string[]) {
-  let names: string[] = args
+/**
+ * Returns a set of initials dervied from the provided names
+ * @param names Names which we need to derive initials from
+ * @returns A single set of initials
+ */
+function getInitials(...names: string[]) {
+  let formatted: string[] = names
     .filter((x) => !isEmpty(x))
     .join(" ")
     .split(" ");
 
-  if (names.length === 1 && isEmpty(names[0])) {
+  if (formatted.length === 1 && isEmpty(formatted[0])) {
     return "U";
   }
 
-  let initials = names[0].substring(0, 1).toUpperCase();
+  let initials = formatted[0].substring(0, 1).toUpperCase();
 
-  if (names.length > 1) {
-    initials += names[names.length - 1].substring(0, 1).toUpperCase();
+  if (formatted.length > 1) {
+    initials += formatted[formatted.length - 1].substring(0, 1).toUpperCase();
   }
   return initials;
 }
