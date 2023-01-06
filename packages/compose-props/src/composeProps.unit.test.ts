5ea17020e6ac426bf7cfe9f7ca2b2ce28412dd17
William Crutchfield <william.r.crutchfield@gmail.com>
2023-01-06 08:33:15 -0500

    Update tests

--- packages/compose-props/src/composeProps.unit.test.ts
+++ packages/compose-props/src/composeProps.unit.test.ts
@@ -5,6 +5,7 @@ describe("composeProps", () => {
     const original = { a: "1" };
     const props = { b: "2" };
     const composed = composeProps(original, props);
+
     expect(composed).toEqual({
       a: "1",
       b: "2",
@@ -12,10 +13,12 @@ describe("composeProps", () => {
   });
 
   test("Compose unique handler props", async () => {
-    const handler = () => console.log("first");
+    const handler = jest.fn();
+
     const original: Record<string, any> = { a: handler };
     const props: Record<string, any> = { b: handler };
     const composed = composeProps(original, props);
+
     expect(composed).toEqual({
       a: handler,
       b: handler,
@@ -23,58 +26,70 @@ describe("composeProps", () => {
   });
 
   test("Compose incompatible handler props", async () => {
-    const logSpy = jest.spyOn(console, "log");
-    const handler = () => console.log("first");
+    const handler = jest.fn();
+
     const original: Record<string, any> = { a: "hello" };
     const props: Record<string, any> = { a: handler };
     const composed = composeProps(original, props);
+
+    const composedSpy = jest.spyOn(composed, "a");
     composed.a();
+
     expect(composed).toEqual({
       a: expect.any(Function),
     });
-    expect(logSpy).toHaveBeenCalledWith("first");
-    expect(logSpy).toHaveBeenCalledTimes(1);
+
+    expect(handler).toHaveBeenCalledTimes(1);
+    expect(composedSpy).toHaveBeenCalledTimes(1);
   });
 
   test("Compose multiple incompatible handler props", async () => {
-    const logSpy = jest.spyOn(console, "log");
-    const handler1 = () => console.log("first");
-    const handler2 = () => console.log("second");
+    const handler1 = jest.fn();
+    const handler2 = jest.fn();
+
     const original: Record<string, any> = { a: handler1 };
     const props1: Record<string, any> = { a: "hello" };
     const props2: Record<string, any> = { a: handler2 };
     const composed = composeProps(original, props1, props2);
+
+    const composedSpy = jest.spyOn(composed, "a");
     composed.a();
+
     expect(composed).toEqual({
       a: expect.any(Function),
     });
-    expect(logSpy).toHaveBeenCalledWith("second");
-    expect(logSpy).toHaveBeenCalledTimes(1);
+    expect(handler1).toHaveBeenCalledTimes(0);
+    expect(handler2).toHaveBeenCalledTimes(1);
+    expect(composedSpy).toHaveBeenCalledTimes(1);
   });
 
   test("Compose handler props more than once", async () => {
-    const logSpy = jest.spyOn(console, "log");
-    const handler1 = () => console.log("first");
-    const handler2 = () => console.log("second");
-    const handler3 = () => console.log("third");
+    const handler1 = jest.fn();
+    const handler2 = jest.fn();
+    const handler3 = jest.fn();
+
     const original: Record<string, any> = { a: handler1 };
     const props1: Record<string, any> = { a: handler2 };
     const props2: Record<string, any> = { a: handler3 };
     const composed = composeProps(original, props1, props2);
+
+    const composedSpy = jest.spyOn(composed, "a");
     composed.a();
+
     expect(composed).toEqual({
       a: expect.any(Function),
     });
-    expect(logSpy).toHaveBeenCalledWith("first");
-    expect(logSpy).toHaveBeenCalledWith("second");
-    expect(logSpy).toHaveBeenCalledWith("third");
-    expect(logSpy).toHaveBeenCalledTimes(3);
+    expect(handler1).toHaveBeenCalledTimes(1);
+    expect(handler2).toHaveBeenCalledTimes(1);
+    expect(handler3).toHaveBeenCalledTimes(1);
+    expect(composedSpy).toHaveBeenCalledTimes(1);
   });
 
   test("Compose className props", async () => {
     const original = { className: "1" };
     const props = { className: "2" };
     const composed = composeProps(original, props);
+
     expect(composed).toEqual({
       className: "1 2",
     });
@@ -84,6 +99,7 @@ describe("composeProps", () => {
     const original = { style: { color: "blue" } };
     const props = { style: { height: "10%" } };
     const composed = composeProps(original, props);
+
     expect(composed).toEqual({
       style: {
         color: "blue",
@@ -96,6 +112,7 @@ describe("composeProps", () => {
     const original = { style: { color: "blue" } };
     const props = { style: { height: "10%", color: "red" } };
     const composed = composeProps(original, props);
+
     expect(composed).toEqual({
       style: {
         color: "red",
