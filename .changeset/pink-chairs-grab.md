e9498f137223577c1b997f9a56f6e7484ea46442
William Crutchfield <william.r.crutchfield@gmail.com>
2023-01-23 15:33:10 -0500

    avatar ref support

---
+++ .changeset/pink-chairs-grab.md
@@ -0,0 +1,5 @@
+---
+"@clutchd/avatar": minor
+---
+
+Support refs
--- packages/avatar/src/avatar.tsx
+++ packages/avatar/src/avatar.tsx
@@ -12,22 +12,22 @@ import {
   IWithSize,
 } from "@clutchd/tailwind";
 import Image from "next/image";
-import { useState } from "react";
+import * as React from "react";
 
 /**
- * Type to define `Avatar` component
+ * Type to define `Avatar` element
  */
-type IAvatar = typeof Avatar;
+type IAvatar = React.ElementRef<typeof Component.span>;
 
 /**
  * Type to define `Avatar` props
  */
 interface IAvatarProps
   extends IWithBorderRadius,
-  IWithBorderWidth,
-  IWithColor,
-  IWithSize,
-  IComponentPropsWithoutRef<typeof Component.div> {
+    IWithBorderWidth,
+    IWithColor,
+    IWithSize,
+    IComponentPropsWithoutRef<typeof Component.span> {
   src: string;
   alt?: string;
 }
@@ -39,64 +39,78 @@ type IAvatarLoadingStates = "idle" | "loading" | "loaded" | "error";
  * @param props `IAvatarProps` used to render this `Avatar`
  * @returns `Avatar` component
  */
-function Avatar({
-  alt = "",
-  borderRadius = "rounded-full",
-  borderWidth = "border",
-  children = "U",
-  className,
-  size = "12",
-  src,
-  theme = "gray",
-  ...props
-}: IAvatarProps) {
-  const [loadingState, setLoadingState] =
-    useState<IAvatarLoadingStates>("idle");
+const Avatar = React.forwardRef<IAvatar, IAvatarProps>(
+  (
+    {
+      alt = "",
+      borderRadius = "rounded-full",
+      borderWidth = "border",
+      children = "U",
+      className,
+      size = "12",
+      src,
+      theme = "gray",
+      ...props
+    },
+    forwardRef
+  ) => {
+    const [loadingState, setLoadingState] =
+      React.useState<IAvatarLoadingStates>("idle");
 
-  const fallback = (
-    <span
-      className={clsx(
-        "flex items-center justify-center w-full h-full font-semibold overflow-hidden",
-        getBgColor(theme, "100"),
-        getTextColor(theme, "500"),
-        borderRadius
-      )}
-    >
-      {children}
-    </span>
-  );
+    function AvatarFallback() {
+      return (
+        <span
+          className={clsx(
+            "flex items-center justify-center w-full h-full font-semibold overflow-hidden",
+            theme && getBgColor(theme, "100"),
+            theme && getTextColor(theme, "500"),
+            borderRadius
+          )}
+        >
+          {children}
+        </span>
+      );
+    }
+    AvatarFallback.displayName = "AvatarFallback";
 
-  const image = (
-    <Image
-      onLoadStart={() => setLoadingState("loading")}
-      onLoadingComplete={() => setLoadingState("loaded")}
-      onError={() => setLoadingState("error")}
-      className={clsx("flex-none object-cover", borderRadius)}
-      height={getSizePx(size)}
-      width={getSizePx(size)}
-      alt={alt}
-      src={src}
-      quality={100}
-    />
-  );
+    function AvatarImage() {
+      return (
+        <Image
+          onLoadStart={() => setLoadingState("loading")}
+          onLoadingComplete={() => setLoadingState("loaded")}
+          onError={() => setLoadingState("error")}
+          className={clsx("flex-none object-cover", borderRadius)}
+          height={getSizePx(size)}
+          width={getSizePx(size)}
+          alt={alt}
+          src={src}
+          quality={100}
+        />
+      );
+    }
+    AvatarImage.displayName = "AvatarImage";
 
-  return (
-    <Component.div
-      className={clsx(
-        "inline-flex items-center justify-center align-middle select-none overflow-hidden",
-        getBorderColor(theme, "300"),
-        getSize(size),
-        borderRadius,
-        borderWidth,
-        className
-      )}
-      {...props}
-    >
-      {src && image}
-      {loadingState !== "loaded" && fallback}
-    </Component.div>
-  );
-}
+    return (
+      <Component.span
+        className={clsx(
+          "inline-flex items-center justify-center align-middle select-none overflow-hidden",
+          getBorderColor(theme, "300"),
+          getSize(size),
+          borderRadius,
+          borderWidth,
+          className
+        )}
+        ref={forwardRef}
+        {...props}
+      >
+        <>
+          {src && <AvatarImage />}
+          {loadingState !== "loaded" && <AvatarFallback />}
+        </>
+      </Component.span>
+    );
+  }
+);
 
 Avatar.displayName = "Avatar";
 
