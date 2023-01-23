b648e87825b15942c4fc4c7ba9384819096fe784
William Crutchfield <william.r.crutchfield@gmail.com>
2023-01-23 16:06:02 -0500

    export IFlex

---
+++ .changeset/fluffy-comics-exist.md
@@ -0,0 +1,5 @@
+---
+"@clutchd/flex": patch
+---
+
+export IFlex
--- packages/flex/src/index.tsx
+++ packages/flex/src/index.tsx
@@ -2,3 +2,4 @@ export { FlexCol } from "./flexCol";
 export type { IFlexCol, IFlexColProps } from "./flexCol";
 export { FlexRow } from "./flexRow";
 export type { IFlexRow, IFlexRowProps } from "./flexRow";
+export type { IFlex } from "./flex";
