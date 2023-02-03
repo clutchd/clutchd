f398d687d9ac958a78a51c8ecc88b5c3dada1e48
William Crutchfield <william.r.crutchfield@gmail.com>
2023-02-02 23:28:08 -0500

    x

--- packages/link/src/link.tsx
+++ packages/link/src/link.tsx
@@ -14,7 +14,7 @@ type ILink = React.ElementRef<typeof Component.a>;
  */
 interface ILinkProps
   extends WithColor,
-    IComponentPropsWithoutRef<typeof Component.a> {}
+  IComponentPropsWithoutRef<typeof Component.a> { }
 
 /**
  * `Link` - A simple link component, leverages next/link
@@ -26,28 +26,28 @@ const Link = React.forwardRef<ILink, ILinkProps>(
     const classNames = twx(
       "cursor-pointer font-medium underline underline-offset-4",
       getTheme(theme, {
-        slate: "text-slate-600 dark:text-slate-500",
-        gray: "text-gray-600 dark:text-gray-500",
-        zinc: "text-zinc-600 dark:text-zinc-500",
-        neutral: "text-neutral-600 dark:text-neutral-500",
-        stone: "text-stone-600 dark:text-stone-500",
-        red: "text-red-600 dark:text-red-500",
-        orange: "text-orange-600 dark:text-orange-500",
-        amber: "text-amber-600 dark:text-amber-500",
-        yellow: "text-yellow-600 dark:text-yellow-500",
-        lime: "text-lime-600 dark:text-lime-500",
-        green: "text-green-600 dark:text-green-500",
-        emerald: "text-emerald-600 dark:text-emerald-500",
-        teal: "text-teal-600 dark:text-teal-500",
-        cyan: "text-cyan-600 dark:text-cyan-500",
-        sky: "text-sky-600 dark:text-sky-500",
-        blue: "text-blue-600 dark:text-blue-500",
-        indigo: "text-indigo-600 dark:text-indigo-500",
-        violet: "text-violet-600 dark:text-violet-500",
-        purple: "text-purple-600 dark:text-purple-500",
-        fuchsia: "text-fuchsia-600 dark:text-fuchsia-500",
-        pink: "text-pink-600 dark:text-pink-500",
-        rose: "text-rose-600 dark:text-rose-500",
+        slate: "text-slate-600 dark:text-slate-400",
+        gray: "text-gray-600 dark:text-gray-400",
+        zinc: "text-zinc-600 dark:text-zinc-400",
+        neutral: "text-neutral-600 dark:text-neutral-400",
+        stone: "text-stone-600 dark:text-stone-400",
+        red: "text-red-600 dark:text-red-400",
+        orange: "text-orange-600 dark:text-orange-400",
+        amber: "text-amber-600 dark:text-amber-400",
+        yellow: "text-yellow-600 dark:text-yellow-400",
+        lime: "text-lime-600 dark:text-lime-400",
+        green: "text-green-600 dark:text-green-400",
+        emerald: "text-emerald-600 dark:text-emerald-400",
+        teal: "text-teal-600 dark:text-teal-400",
+        cyan: "text-cyan-600 dark:text-cyan-400",
+        sky: "text-sky-600 dark:text-sky-400",
+        blue: "text-blue-600 dark:text-blue-400",
+        indigo: "text-indigo-600 dark:text-indigo-400",
+        violet: "text-violet-600 dark:text-violet-400",
+        purple: "text-purple-600 dark:text-purple-400",
+        fuchsia: "text-fuchsia-600 dark:text-fuchsia-400",
+        pink: "text-pink-600 dark:text-pink-400",
+        rose: "text-rose-600 dark:text-rose-400",
       }),
       className
     );
