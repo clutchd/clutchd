1417f6433dbd3753bc51fc3ec3aff440b5591103
William Crutchfield <william.r.crutchfield@gmail.com>
2023-02-03 00:16:18 -0500

    x

---
+++ .changeset/twenty-lizards-nail.md
@@ -0,0 +1,5 @@
+---
+"@clutchd/input": minor
+---
+
+Add theme support, improve class merge
--- packages/input/src/input.tsx
+++ packages/input/src/input.tsx
@@ -1,6 +1,7 @@
-import { clsx } from "@clutchd/clsx";
+import { twx } from "@clutchd/twx";
 import { Component, IComponentPropsWithoutRef } from "@clutchd/component";
 import { composeEventHandlers } from "@clutchd/compose-event-handlers";
+import { getGrayTheme, WithGrayColor } from "@clutchd/tailwind";
 import * as React from "react";
 import { IInputInfoProps, InputInfo } from "./inputInfo";
 
@@ -14,7 +15,8 @@ type IInput = React.ElementRef<typeof Component.input>;
  */
 interface IInputProps
   extends IInputInfoProps,
-    IComponentPropsWithoutRef<typeof Component.input> {}
+  WithGrayColor,
+  IComponentPropsWithoutRef<typeof Component.input> { }
 
 /**
  * Type to define possible `Input` validation states
@@ -27,7 +29,10 @@ type IInputValidationStates = "valid" | "invalid" | "fixed" | undefined;
  * @returns `Input` component
  */
 const Input = React.forwardRef<IInput, IInputProps>(
-  ({ className, error, helpText, onBlur, ...props }, forwardedRef) => {
+  (
+    { className, error, helpText, onBlur, theme = "gray", ...props },
+    forwardedRef
+  ) => {
     // track validation state of this input
     const [validated, setValidated] =
       React.useState<IInputValidationStates>(undefined);
@@ -72,7 +77,19 @@ const Input = React.forwardRef<IInput, IInputProps>(
 
         default:
         case "valid":
-          return "text-gray-700 dark:text-gray-200 border-gray-300 dark:border-gray-700 focus:ring-blue-500/50 focus:dark:ring-blue-400/50 focus:border-blue-500 dark:focus:border-blue-400";
+          return twx(
+            "focus:ring-blue-500/50 focus:dark:ring-blue-400/50 focus:border-blue-500 dark:focus:border-blue-400",
+            getGrayTheme(theme, {
+              slate:
+                "text-slate-700 dark:text-slate-200 border-slate-300 dark:border-slate-700",
+              gray: "text-gray-700 dark:text-gray-200 border-gray-300 dark:border-gray-700",
+              zinc: "text-zinc-700 dark:text-zinc-200 border-zinc-300 dark:border-zinc-700",
+              neutral:
+                "text-neutral-700 dark:text-neutral-200 border-neutral-300 dark:border-neutral-700",
+              stone:
+                "text-stone-700 dark:text-stone-200 border-stone-300 dark:border-stone-700",
+            })
+          );
       }
     }
 
@@ -80,8 +97,18 @@ const Input = React.forwardRef<IInput, IInputProps>(
       <>
         <Component.input
           aria-invalid={validated === "invalid" ? "true" : "false"}
-          className={clsx(
-            "flex w-full px-2 py-1 my-1 font-normal border rounded-md shadow-sm appearance-none text-small focus:outline-none focus:ring bg-gray-50 dark:bg-gray-800 placeholder:text-gray-400 placeholder:dark:text-gray-500 disabled:border-gray-300/50 disabled:dark:border-gray-700/50 disabled:cursor-not-allowed disabled:shadow-none disabled:bg-gray-50/50 dark:disabled:bg-gray-800/50 disabled:text-gray-700/50 disabled:dark:text-gray-200/50 read-only:border-gray-300/50 read-only:dark:border-gray-700/50 read-only:bg-gray-50/50 read-only:dark:bg-gray-800/50 read-only:shadow-none invalid:focus:bg-red-50 invalid:dark:focus:bg-red-500/10 invalid:text-red-600 invalid:dark:text-red-300 invalid:border-red-300 invalid:dark:border-red-700 invalid:focus:ring-red-500/50 invalid:focus:border-red-500",
+          className={twx(
+            "flex w-full px-2 py-1 my-1 font-normal border rounded-md shadow-sm appearance-none text-small focus:outline-none focus:ring  disabled:cursor-not-allowed disabled:shadow-none read-only:shadow-none invalid:focus:bg-red-50 invalid:dark:focus:bg-red-500/10 invalid:text-red-600 invalid:dark:text-red-300 invalid:border-red-300 invalid:dark:border-red-700 invalid:focus:ring-red-500/50 invalid:focus:border-red-500",
+            getGrayTheme(theme, {
+              slate:
+                "bg-slate-50 dark:bg-slate-800 placeholder:text-slate-400 placeholder:dark:text-slate-500 disabled:border-slate-300/50 disabled:dark:border-slate-700/50 disabled:bg-slate-50/50 dark:disabled:bg-slate-800/50 disabled:text-slate-700/50 disabled:dark:text-slate-200/50 read-only:border-slate-300/50 read-only:dark:border-slate-700/50 read-only:bg-slate-50/50 read-only:dark:bg-slate-800/50",
+              gray: "bg-gray-50 dark:bg-gray-800 placeholder:text-gray-400 placeholder:dark:text-gray-500 disabled:border-gray-300/50 disabled:dark:border-gray-700/50 disabled:bg-gray-50/50 dark:disabled:bg-gray-800/50 disabled:text-gray-700/50 disabled:dark:text-gray-200/50 read-only:border-gray-300/50 read-only:dark:border-gray-700/50 read-only:bg-gray-50/50 read-only:dark:bg-gray-800/50",
+              zinc: "bg-zinc-50 dark:bg-zinc-800 placeholder:text-zinc-400 placeholder:dark:text-zinc-500 disabled:border-zinc-300/50 disabled:dark:border-zinc-700/50 disabled:bg-zinc-50/50 dark:disabled:bg-zinc-800/50 disabled:text-zinc-700/50 disabled:dark:text-zinc-200/50 read-only:border-zinc-300/50 read-only:dark:border-zinc-700/50 read-only:bg-zinc-50/50 read-only:dark:bg-zinc-800/50",
+              neutral:
+                "bg-neutral-50 dark:bg-neutral-800 placeholder:text-neutral-400 placeholder:dark:text-neutral-500 disabled:border-neutral-300/50 disabled:dark:border-neutral-700/50 disabled:bg-neutral-50/50 dark:disabled:bg-neutral-800/50 disabled:text-neutral-700/50 disabled:dark:text-neutral-200/50 read-only:border-neutral-300/50 read-only:dark:border-neutral-700/50 read-only:bg-neutral-50/50 read-only:dark:bg-neutral-800/50",
+              stone:
+                "bg-stone-50 dark:bg-stone-800 placeholder:text-stone-400 placeholder:dark:text-stone-500 disabled:border-stone-300/50 disabled:dark:border-stone-700/50 disabled:bg-stone-50/50 dark:disabled:bg-stone-800/50 disabled:text-stone-700/50 disabled:dark:text-stone-200/50 read-only:border-stone-300/50 read-only:dark:border-stone-700/50 read-only:bg-stone-50/50 read-only:dark:bg-stone-800/50",
+            }),
             getVariantStyles(validated),
             className
           )}
