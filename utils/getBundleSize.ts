864b0abeae0eea2f103f24475128c79d9498f5f3
William Crutchfield <william.r.crutchfield@gmail.com>
2023-05-27 17:33:42 -0700

    add bundle size util

---
+++ utils/getBundleSize.ts
@@ -0,0 +1,39 @@
+import fs from "fs";
+import fetch from "node-fetch";
+import { minify } from "terser";
+import zlib from "zlib";
+
+export async function getLocalSize(path: string) {
+  const input = fs.readFileSync(path, "utf8");
+  const result = await minify(input, {
+    module: true,
+    compress: true,
+  });
+
+  // @ts-ignore
+  return zlib.gzipSync(result.code).byteLength;
+}
+
+//   /**
+//    * Get the size of a package from bundlejs.com
+//    * @param pkg Package name we are looking for
+//    * @returns
+//    */
+//   export async function getRemoteSize(pkg: string) {
+//     // @ts-ignore
+//     const data = await fetch(`https://edge.bundlejs.com/?q=${pkg}`);
+//     const { size } = await data.json();
+//     return size?.rawCompressedSize;
+//   }
+
+/**
+ * Get the size of a package from bundlephobia.com
+ * @param pkg Package name we are looking for
+ * @returns
+ */
+export async function getRemoteSize(pkg: string) {
+  // @ts-ignore
+  const data = await fetch(`https://bundlephobia.com/api/size?package=${pkg}`);
+  const { size } = await data.json();
+  return size;
+}
