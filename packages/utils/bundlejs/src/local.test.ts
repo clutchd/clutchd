634ad872b8c878520b2430b9709904cbd45163fa
William Crutchfield <william.r.crutchfield@gmail.com>
2023-05-31 13:10:52 -0700

    fix bundlejs

--- packages/utils/bundlejs/src/local.test.ts
+++ packages/utils/bundlejs/src/local.test.ts
@@ -1,9 +1,8 @@
-// @ts-ignore
-import fs from "fs";
+import { readFileSync } from "fs";
 import { getLocalSize } from ".";
 
 test("ensures the local size compression functions", async () => {
-  const input = fs.readFileSync("dist/index.js", "utf8");
+  const input = readFileSync("dist/index.js", "utf8");
   const size = await getLocalSize([input]);
 
   expect(size.rawCompressedSize).toBeLessThanOrEqual(size.rawUncompressedSize);
