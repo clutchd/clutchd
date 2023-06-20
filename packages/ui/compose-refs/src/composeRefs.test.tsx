465de047e86806db5afd24c6218f521474f4bd1e
William Crutchfield <william.r.crutchfield@gmail.com>
2023-06-20 15:36:48 -0700

    update jest

--- packages/ui/compose-refs/src/composeRefs.test.tsx
+++ packages/ui/compose-refs/src/composeRefs.test.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { composeRefs, useComposedRefs } from ".";
-import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 
 test("composeRefs", () => {
