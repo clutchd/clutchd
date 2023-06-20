3d876dcdef65c66e2d0902fb562ec3a4e9bdd4d6
William Crutchfield <william.r.crutchfield@gmail.com>
2023-06-20 15:31:07 -0700

    update config

---
+++ .changeset/funny-countries-live.md
@@ -0,0 +1,5 @@
+---
+"@clutchd/config": minor
+---
+
+Migrate to @testing-library/jest-dom
--- packages/config/jsdom.cjs
+++ packages/config/jsdom.cjs
@@ -1,26 +1,26 @@
 // A custom jest environment for jsdom that fixes the missing TextEncoder and TextDecoder.
 
-'use strict';
+"use strict";
 
-const { TextEncoder, TextDecoder } = require('util');
-const { default: $JSDOMEnvironment, TestEnvironment } = require('jest-environment-jsdom');
+const { TextEncoder, TextDecoder } = require("util");
+const {
+  default: $JSDOMEnvironment,
+  TestEnvironment,
+} = require("@testing-library/jest-dom");
 
-Object.defineProperty(exports, '__esModule', {
-  value: true
+Object.defineProperty(exports, "__esModule", {
+  value: true,
 });
 
 class JSDOMEnvironment extends $JSDOMEnvironment {
   constructor(...args) {
     const { global } = super(...args);
-    if ( !global.TextEncoder )
-      global.TextEncoder = TextEncoder;
-    if ( !global.TextDecoder )
-      global.TextDecoder = TextDecoder;
-    if ( !global.Uint8Array )
-      global.Uint8Array = Uint8Array;
+    if (!global.TextEncoder) global.TextEncoder = TextEncoder;
+    if (!global.TextDecoder) global.TextDecoder = TextDecoder;
+    if (!global.Uint8Array) global.Uint8Array = Uint8Array;
   }
 }
 
 exports.default = JSDOMEnvironment;
-exports.TestEnvironment = TestEnvironment === $JSDOMEnvironment ?
-                            JSDOMEnvironment : TestEnvironment;
+exports.TestEnvironment =
+  TestEnvironment === $JSDOMEnvironment ? JSDOMEnvironment : TestEnvironment;
