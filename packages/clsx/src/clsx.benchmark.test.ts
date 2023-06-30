6d2a3cc19f1a6bbbf2bfac5f3f9144e823ce562b
William Crutchfield <william.r.crutchfield@gmail.com>
2023-06-30 14:27:06 -0700

    remove benchmark requirement

--- packages/clsx/src/clsx.benchmark.test.ts
+++ packages/clsx/src/clsx.benchmark.test.ts
@@ -1,50 +1,85 @@
-let data = {};
+let data;
 
 try {
   data = require("./clsx.benchmark.json");
 } catch (e) {
-  console.error(
+  console.warn(
     "clsx.benchmark.json not found. Please run `pnpm run benchmark` first."
   );
 }
 
 test("strings", () => {
+  if (!data) {
+    console.warn("skipping benchmark tests.");
+    return;
+  }
+
   expect(parseFloat(data?.["strings"]?.["clutchd"])).toBeGreaterThanOrEqual(
     parseFloat(data?.["strings"]?.["clsx"])
   );
 });
 
 test("objects", () => {
+  if (!data) {
+    console.warn("skipping benchmark tests.");
+    return;
+  }
+
   expect(parseFloat(data?.["objects"]?.["clutchd"])).toBeGreaterThanOrEqual(
     parseFloat(data?.["objects"]?.["clsx"])
   );
 });
 
 test("arrays", () => {
+  if (!data) {
+    console.warn("skipping benchmark tests.");
+    return;
+  }
+
   expect(parseFloat(data?.["arrays"]?.["clutchd"])).toBeGreaterThanOrEqual(
     parseFloat(data?.["arrays"]?.["clsx"])
   );
 });
 
 test("nested_arrays", () => {
+  if (!data) {
+    console.warn("skipping benchmark tests.");
+    return;
+  }
+
   expect(
     parseFloat(data?.["nested_arrays"]?.["clutchd"])
   ).toBeGreaterThanOrEqual(parseFloat(data?.["nested_arrays"]?.["clsx"]));
 });
 
 test("nested_objects", () => {
+  if (!data) {
+    console.warn("skipping benchmark tests.");
+    return;
+  }
+
   expect(
     parseFloat(data?.["nested_objects"]?.["clutchd"])
   ).toBeGreaterThanOrEqual(parseFloat(data?.["nested_objects"]?.["clsx"]));
 });
 
 test("mixed", () => {
+  if (!data) {
+    console.warn("skipping benchmark tests.");
+    return;
+  }
+
   expect(parseFloat(data?.["mixed"]?.["clutchd"])).toBeGreaterThanOrEqual(
     parseFloat(data?.["mixed"]?.["clsx"])
   );
 });
 
 test("mixed_bad", () => {
+  if (!data) {
+    console.warn("skipping benchmark tests.");
+    return;
+  }
+
   expect(parseFloat(data?.["mixed_bad"]?.["clutchd"])).toBeGreaterThanOrEqual(
     parseFloat(data?.["mixed_bad"]?.["clsx"])
   );
