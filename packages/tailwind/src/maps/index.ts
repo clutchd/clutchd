1ab40604c310d9bed125e99ba3eda869e9a1f82f
William Crutchfield <william.r.crutchfield@gmail.com>
2023-07-10 14:27:47 -0700

    remove old index files

--- packages/tailwind/src/maps/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./grid.column.start.end.map";
-export * from "./grid.row.start.end.map";
-export * from "./grid.template.columns.map";
-export * from "./height.map";
-export * from "./margin.map";
-export * from "./width.map";
--- packages/tailwind/src/typography/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./font.size";
-export * from "./font.weight";
-export * from "./line.height";
