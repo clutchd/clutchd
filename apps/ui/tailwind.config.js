a8b7e81b0f827014bc737699ec23b2de7afc2222
William Crutchfield <william.r.crutchfield@gmail.com>
2023-07-10 17:05:30 -0700

    parse clutchd styles

--- apps/ui/tailwind.config.js
+++ apps/ui/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/@clutchd/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
