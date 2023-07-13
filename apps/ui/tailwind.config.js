f8d35f893787411beb9b9fd7e181227a58d81b9c
William Crutchfield <william.r.crutchfield@gmail.com>
2023-07-13 11:15:46 -0700

    prep for final reworks

--- apps/ui/tailwind.config.js
+++ apps/ui/tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   content: [
     "./node_modules/@clutchd/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class",
   plugins: [],
 };
