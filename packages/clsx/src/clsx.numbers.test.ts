2da486ffc5a659af32db577c6319c439cd811b12
William Crutchfield <william.r.crutchfield@gmail.com>
2023-08-20 08:33:21 -0700

    Update clsx

---
+++ packages/clsx/src/clsx.numbers.test.ts
@@ -0,0 +1,16 @@
+import { clsx } from ".";
+
+test("numbers", () => {
+  expect(clsx(1)).toEqual("1");
+  expect(clsx(12)).toEqual("12");
+  expect(clsx(0.1)).toEqual("0.1");
+  expect(clsx(0)).toEqual("");
+
+  expect(clsx(Infinity)).toEqual("Infinity");
+  expect(clsx(NaN)).toEqual("");
+});
+
+test("numbers (variadic)", () => {
+  expect(clsx(0, 1)).toEqual("1");
+  expect(clsx(1, 2)).toEqual("1 2");
+});
