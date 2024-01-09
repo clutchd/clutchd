5bf1b5a3ede0fb90bbe5e6f5eb69c3e4491f4aab
William Crutchfield <william.r.crutchfield@gmail.com>
2024-01-08 17:16:44 -0800

    x

--- packages/bundlejs/src/remote.test.ts
+++ packages/bundlejs/src/remote.test.ts
@@ -1,6 +1,11 @@
 import { getRemoteSize } from ".";
 
-test("ensures the local size compression functions", async () => {
+test("ensures the remote size compression functions with bundlephobia", async () => {
   const size = await getRemoteSize("@clutchd/clsx");
+  expect(size.rawCompressedSize).toBeLessThanOrEqual(size.rawUncompressedSize);
+});
+
+test("ensures the remote size compression functions with bundlejs", async () => {
+  const size = await getRemoteSize("@clutchd/clsx", "bundlejs");
   expect(size.rawCompressedSize).toBeLessThanOrEqual(size.rawUncompressedSize);
 });
