f7e2055e371092ff2df920713c0428ead3258140
William Crutchfield <william.r.crutchfield@gmail.com>
2024-01-10 16:47:22 -0800

    include all types from @clutchd packages

---
+++ packages/skeleton/preload.ts
@@ -0,0 +1,3 @@
+import "@types/bun-jest-dom";
+import { GlobalRegistrator } from "@happy-dom/global-registrator";
+GlobalRegistrator.register();
--- packages/skeleton/src/skeleton.test.tsx
+++ packages/skeleton/src/skeleton.test.tsx
@@ -1,4 +1,5 @@
 import { render } from "@testing-library/react";
+import { expect, test } from "bun:test";
 import React from "react";
 import { Skeleton } from ".";
 
