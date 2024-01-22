a0ed544efbdede5db0fb0e1a66bdcb7708515c38
William Crutchfield <william.r.crutchfield@gmail.com>
2024-01-21 21:59:25 -0800

    x

--- packages/utils/tailwind/src/index.ts
+++ packages/utils/tailwind/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./borders";
 export * from "./flex.grid";
+export * from "./layout";
 export * from "./sizing";
 export * from "./spacing";
 export * from "./typography";
---
+++ packages/utils/tailwind/src/layout/display.ts
@@ -0,0 +1,31 @@
+/**
+ * Display
+ * @see https://tailwindcss.com/docs/display
+ */
+
+export type Display =
+  | "block"
+  | "inline-block"
+  | "inline"
+  | "flex"
+  | "inline-flex"
+  | "table"
+  | "inline-table"
+  | "table-caption"
+  | "table-cell"
+  | "table-column"
+  | "table-column-group"
+  | "table-footer-group"
+  | "table-header-group"
+  | "table-row-group"
+  | "table-row"
+  | "flow-root"
+  | "grid"
+  | "inline-grid"
+  | "contents"
+  | "list-item"
+  | "hidden";
+
+export interface WithDisplay {
+  display?: Display;
+}
