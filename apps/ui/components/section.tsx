22f0ac1e514118e06c3ae4fa1d3634926ab8dbe3
William Crutchfield <william.r.crutchfield@gmail.com>
2024-02-19 09:47:24 -0800

    x

--- apps/ui/components/section.tsx
+++ apps/ui/components/section.tsx
@@ -11,7 +11,7 @@ function Section({
   return (
     <div className="mb-10">
       <h1 className="pb-4 text-4xl font-extrabold">{title}</h1>
-      <Divider className="bg-gray-950" />
+      <Divider className="bg-gray-950 data-[orientation=horizontal]:h-px data-[orientation=vertical]:w-px" />
       {children}
     </div>
   );
