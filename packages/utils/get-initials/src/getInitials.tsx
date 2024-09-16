ba4b292bac6307aed1b82c2795c17a3256d33385
William Crutchfield <william.r.crutchfield@gmail.com>
2024-09-15 20:09:22 -0700

    x

--- packages/utils/get-initials/src/getInitials.tsx
+++ packages/utils/get-initials/src/getInitials.tsx
@@ -5,27 +5,26 @@
  */
 function getInitials(...names: string[]) {
   // declare vars to hold initials
-  let l,
-    f = "U";
+  let f = "U";
 
   // filter out all invalid inputs
-  names = names
+  const result = names
     .filter((x) => typeof x === "string")
     .join(" ")
     .split(" ")
-    .filter((x) => x.trim() != "");
+    .filter((x) => x.trim() !== "");
 
   // if no valid inputs provided, return default
-  if (names.length === 0) {
+  if (result.length === 0) {
     return f;
   }
 
   // otherwise, get current length of array
-  let len = names.length;
+  const len = result.length;
 
   // collect first initial
-  if (names[0]) {
-    f = names[0].charAt(0);
+  if (result[0]) {
+    f = result[0].charAt(0);
   }
 
   // if only one valid input, return first initial
@@ -34,7 +33,7 @@ function getInitials(...names: string[]) {
   }
 
   // otherwise, return first and last initial
-  return (f + names[len - 1]?.charAt(0)).toUpperCase();
+  return (f + result[len - 1]?.charAt(0)).toUpperCase();
 }
 
 export { getInitials };
