076028921547f55a0d5c5081ed5228f73687019f
William Crutchfield <william.r.crutchfield@gmail.com>
2024-11-11 12:59:09 -0800

    block

---
+++ packages/block/src/block.test.tsx
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+import { expect, test } from "bun:test";
+import { Block } from ".";
+
+test("render block", () => {
+  const { container } = render(<Block />);
+
+  const div = container.querySelector("div");
+
+  expect(container.hasChildNodes()).toEqual(true);
+  expect(div).toBeInTheDocument();
+  // TODO: FIX TEST => expect(div).toHaveClass("block");
+});
---
+++ packages/block/src/index.tsx
@@ -0,0 +1,2 @@
+export { Block } from "./block";
+export type { IBlock, IBlockProps, IBlockHtmlProps } from "./block";
