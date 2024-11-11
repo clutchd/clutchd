3a834496f75ea6987d14a66576aae052d1fefe89
William Crutchfield <william.r.crutchfield@gmail.com>
2024-11-11 15:21:07 -0800

    lint

--- packages/www/app/layout.tsx
+++ packages/www/app/layout.tsx
@@ -21,9 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={manrope.variable}>
       <Page asChild className="bg-white">
-        <body>
-          {children}
-        </body>
+        <body>{children}</body>
       </Page>
     </html>
   );
