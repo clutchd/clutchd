1fdf8528460020b93dbf69d4c42ff0f9f0c5126a
William Crutchfield <william.r.crutchfield@gmail.com>
2024-11-12 08:50:44 -0800

    lint

--- scripts/replace-workspace-protocol.ts
+++ scripts/replace-workspace-protocol.ts
@@ -1,29 +1,39 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("node:fs");
+const path = require("node:path");
 
 function replaceWorkspaceProtocol(packagePath) {
-  const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-  const workspaceRoot = path.resolve(__dirname, '..');
+  const packageJson = JSON.parse(fs.readFileSync(packagePath, "utf8"));
+  const workspaceRoot = path.resolve(__dirname, "..");
 
-  ['dependencies', 'devDependencies'].forEach(depType => {
+  for (const depType of ["dependencies", "devDependencies"]) {
     if (packageJson[depType]) {
-      Object.keys(packageJson[depType]).forEach(dep => {
-        if (packageJson[depType][dep] === 'workspace:*') {
-          const depPackageJson = JSON.parse(fs.readFileSync(path.join(workspaceRoot, 'packages', dep.split('/')[1], 'package.json'), 'utf8'));
+      for (const dep of Object.keys(packageJson[depType])) {
+        if (packageJson[depType][dep] === "workspace:*") {
+          const depPackageJson = JSON.parse(
+            fs.readFileSync(
+              path.join(
+                workspaceRoot,
+                "packages",
+                dep.split("/")[1],
+                "package.json",
+              ),
+              "utf8",
+            ),
+          );
           packageJson[depType][dep] = `${depPackageJson.version}`;
         }
-      });
+      }
     }
-  });
+  }
 
   fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
 }
 
 // Replace in all workspace packages
-const packagesDir = path.join(__dirname, '..', 'packages');
-fs.readdirSync(packagesDir).forEach(packageName => {
-  const packageJsonPath = path.join(packagesDir, packageName, 'package.json');
+const packagesDir = path.join(__dirname, "..", "packages");
+for (const packageName of fs.readdirSync(packagesDir)) {
+  const packageJsonPath = path.join(packagesDir, packageName, "package.json");
   if (fs.existsSync(packageJsonPath)) {
     replaceWorkspaceProtocol(packageJsonPath);
   }
-});
+}
