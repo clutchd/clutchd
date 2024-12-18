0fa7b0f773311bf8ecb3b662a3c5ba9afeaecd89
William Crutchfield <william.r.crutchfield@gmail.com>
2024-12-18 15:20:07 -0800

    use clsx

--- packages/clsx/src/clsx.exports.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "bun:test";
-import { clsx } from ".";
-
-test("exports", () => {
-  expect(typeof clsx).toEqual("function");
-  expect(typeof clsx()).toEqual("string");
-});
--- packages/clsx/src/clsx.functions.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "bun:test";
-import { clsx } from ".";
-
-test("functions", () => {
-  const foo = () => {};
-  expect(clsx(foo, "hello")).toEqual("hello");
-  expect(clsx(foo, "hello", clsx)).toEqual("hello");
-  expect(clsx(foo, "hello", [[clsx], "world"])).toEqual("hello world");
-});
