73ca6c66fbaf76b21bfd95b5a3530e5bf1b2b008
William Crutchfield <william.r.crutchfield@gmail.com>
2024-12-18 14:53:51 -0800

    pugre link (nextjs default link with "/" default href)

--- packages/link/src/index.tsx
+++
@@ -1,4 +0,0 @@
-export { Link } from "./link";
-export type { ILink, ILinkHtmlProps, ILinkProps } from "./link";
-export { LinkRoot } from "./linkRoot";
-export type { ILinkRoot, ILinkRootHtmlProps, ILinkRootProps } from "./linkRoot";
