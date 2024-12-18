288354c92fe3ef3817ec9b788e4e468ebe690155
William Crutchfield <william.r.crutchfield@gmail.com>
2024-12-18 15:19:06 -0800

    use @radix-ui/react-compose-refs if needed

--- packages/compose-refs/src/composeRefs.exports.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "bun:test";
-import { composeRefs } from ".";
-
-test("exports", () => {
-  expect(typeof composeRefs).toEqual("function");
-  expect(typeof composeRefs(() => {})).toEqual("function");
-});
