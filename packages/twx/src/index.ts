11dc3c61a99da3a63d038b0b8916266621eb2b4c
William Crutchfield <william.r.crutchfield@gmail.com>
2024-12-19 21:46:14 -0800

    make twx accept unknowns

--- packages/twx/src/index.ts
+++ packages/twx/src/index.ts
@@ -1,6 +1,6 @@
 import clsx, { type ClassValue } from "clsx/lite";
 import { twMerge } from "tailwind-merge";
 
-export function twx(...inputs: ClassValue[]) {
-  return twMerge(clsx(...inputs));
+export function twx(...inputs: unknown[]) {
+  return twMerge(clsx(...inputs as ClassValue[]));
 }
