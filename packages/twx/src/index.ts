fb8ee9a414ec6c1a96e0f393a898c8c3591b9b55
William Crutchfield <william.r.crutchfield@gmail.com>
2024-12-23 09:35:43 -0800

    twx format

--- packages/twx/src/index.ts
+++ packages/twx/src/index.ts
@@ -2,5 +2,5 @@ import clsx, { type ClassValue } from "clsx/lite";
 import { twMerge } from "tailwind-merge";
 
 export function twx(...inputs: unknown[]) {
-  return twMerge(clsx(...inputs as ClassValue[]));
+  return twMerge(clsx(...(inputs as ClassValue[])));
 }
